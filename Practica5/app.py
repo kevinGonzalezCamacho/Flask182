3a436877bdebfbc2fa2884b22b3b75b11957a82e
kevinGonzalezCamacho <133286128+kevinGonzalezCamacho@users.noreply.github.com>
2023-06-20 18:52:32 -0600

    Insert a la BD, Redirect y Mensajes a la Vista

--- Practica5/app.py
+++ Practica5/app.py
@@ -1,46 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
 
-from flask import Flask,render_template,request
+#inicializacion del servido Flask
+app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "dbflask"  
 
-#inicializacion del servido Flask 
-app= Flask(__name__)
-
-#configurarciones de conexion a BD
-app.config['MYSQL_HOST']= "localhost"
-app.config['MYSQL_USER']= "root"
-app.config['MYSQL_PASSWORD']= "123456"
-app.config['MYSQL_BD']= "bdflask"
-
-
+app.secret_key='mysecretkey'
+mysql = MySQL(app)
 
 #Declaracion de rutas
-
 
 # ruta se compone del nombre y funcion
 
 # Ruta Index http://localhost:5000
 @app.route('/')
 def index():
-    return render_template ( 'index.html')
+    return render_template('index.html')
 
-@app.route('/guardar',methods=['POST'])
+@app.route('/guardar', methods=['POST'])
 def guardar():
     if request.method == 'POST':
-        titulo = request.form['txtTitulo']
-        artista = request.form['txtArtista']
-        anio = request.form['txtAnio']
-        print(titulo,artista,anio)
-         
-
-    return ("La inf del album llego a su ruta amigo ;")
+        Vtitulo = request.form['txtTitulo']
+        Vartista = request.form['txtArtista']
+        Vanio = request.form['txtAnio']
+        
+        cs = mysql.connection.cursor()
+        cs.execute('INSERT INTO albums (titulo, artista, anio) VALUES (%s, %s, %s)', (Vtitulo, Vartista, Vanio))
+        mysql.connection.commit()
+        cs.close()
+        
+        flash('Album Agregado Correctamente')
+    return redirect(url_for('index'))
 
 @app.route('/eliminar')
 def eliminar():
-    return "se elimino el album en la BD"
+    return "Se eliminó el álbum de la base de datos."
 
 # Ejecucion de servidor 
-if __name__==  '__main__':
-        app.run(port= 5000,debug=True)
-
-
-
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
