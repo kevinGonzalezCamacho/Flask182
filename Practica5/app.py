a3ecf29265aced3ed7a73414566fe0e020202594
kevinGonzalezCamacho <133286128+kevinGonzalezCamacho@users.noreply.github.com>
2023-07-04 18:55:27 -0600

    pract8: Actualización completa

--- Practica5/app.py
+++ Practica5/app.py
@@ -41,6 +41,33 @@ def guardar():
         flash('Album Agregado Correctamente')
     return redirect(url_for('index'))
 
+
+
+@app.route('/editar/<id>')
+def editar(id):
+    curEditar= mysql.connection.cursor()
+    curEditar.execute('select * from albums where id = %s ', (id,))
+    consulId= curEditar.fetchone()
+    
+    return render_template('editarAlbum.html',album= consulId)
+
+
+@app.route('/actualizar/<id>', methods=['POST'])
+def actualizar(id):
+    
+    if request.method == 'POST':
+        Vtitulo = request.form['txtTitulo']
+        Vartista = request.form['txtArtista']
+        Vanio = request.form['txtAnio']
+
+        curAct= mysql.connection.cursor()
+        curAct.execute('update albums set titulo=%s, artista=%s, anio=%s where id=%s',(Vtitulo,Vartista,Vanio,id ))
+        mysql.connection.commit()
+         
+    flash('Album Actualizado en BD')
+    return redirect(url_for('index'))
+    
+
 @app.route('/eliminar')
 def eliminar():
     return "Se eliminó el álbum de la base de datos."
